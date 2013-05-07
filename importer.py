dc2012cc39619c5f2a9f44089db5a297788835b7
Shane Kwon <bridgetjonesdiaryluvr@gmail.com>
2013-05-06 19:04:18 -0700

    no more duplicates thanks to indexing

--- importer.py
+++ importer.py
@@ -3,6 +3,7 @@
 import dpkt
 import sys
 import db_neo4j
+from py2neo import neo4j, cypher
 
 def str_from_mac(mac):
   """Returns a string representing a given MAC"""
@@ -13,11 +14,16 @@ pcap = dpkt.pcap.Reader(file)
 
 db = db_neo4j.db_neo4j()
 
-n = 1
+#n = 1
 for time_stamp, buf in pcap:
   eth = dpkt.ethernet.Ethernet(buf)
-  db.insert_nodes_relation({"mac": str_from_mac(eth.src)}, {"mac": str_from_mac(eth.dst)}, str(n))
-  print str_from_mac(eth.src), str_from_mac(eth.dst), str(n)
-  n += 1
+#  db.insert_nodes_relation({"mac": str_from_mac(eth.src)}, {"mac": str_from_mac(eth.dst)}, str(n))
+#  print str_from_mac(eth.src), str_from_mac(eth.dst), str(n)
+#  n += 1
 
-f.close()
+  macaddr = db.graph_db.get_or_create_index(neo4j.Node, "MACAddress")
+  src = db.graph_db.get_or_create_indexed_node("MAC Address", "mac", str_from_mac(eth.src), {"mac": str_from_mac(eth.src)})
+  dest = macaddr.get_or_create("mac", str_from_mac(eth.dst), {"mac": str_from_mac(eth.dst)})
+  db.graph_db.create((src, "TRAFFIC_TO", dest))
+
+#f.close()
